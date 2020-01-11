365cbff4a5ae8f8a2248c84ed9be71b8f52cf2f7
mauriciohike <mauriciorenanvs@gmail.com>
2020-01-11 18:57:49 -0300

    chore: containers start

---
+++ src/layouts/ContainerInside/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ContainerInside = () => {
+  return (
+    <div />
+  );
+}
+
+export default ContainerInside;
---
+++ src/layouts/ContainerOutside/styles.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ContainerOutside = () => {
+  return (
+    <div />
+  );
+}
+
+export default ContainerOutside;
